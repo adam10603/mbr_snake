accc586611dd25e27c8e4286b011c7cf4e8198b6
Adam Dobos <adam10603@gmail.com>
2021-03-06 23:50:59 +0100

    first commit

---
+++ makefile
@@ -0,0 +1,64 @@
+# Terrible makefile
+
+SHELL:=/bin/bash
+
+CXX = g++
+CXX_FLAGS = -std=c++17 -Os -m16 -march=i386 -ffreestanding -fmerge-constants -c
+LD = ld
+LD_FLAGS = -static --nmagic -nostdlib -m elf_i386
+
+EXEC = mbr_snake
+SOURCE = $(EXEC).cpp
+SOURCE_DIR = src
+OBJECT = $(EXEC).o
+OBJECT_DIR = obj
+OUTPUT_DIR = bin
+OUTPUT = $(EXEC).bin
+OUTPUT_BS = $(EXEC)_bs.bin
+OUTPUT_IMG = $(EXEC).img
+
+# Main target
+$(EXEC): $(OBJECT)
+	@ld $(LD_FLAGS) -Tlinker_nosig.ld -o $(OUTPUT_DIR)/$(EXEC).elf $(OBJECT_DIR)/$(OBJECT)
+	@objcopy -O binary $(OUTPUT_DIR)/$(EXEC).elf $(OUTPUT_DIR)/$(OUTPUT)
+	@rm $(OUTPUT_DIR)/$(EXEC).elf
+	@SNAKE_EXEC_SIZE=$$(stat -L -c %s $(OUTPUT_DIR)/$(OUTPUT)) ; \
+	if [ $$SNAKE_EXEC_SIZE -gt 510 ] ; then \
+		printf "Binary is too large: $$SNAKE_EXEC_SIZE bytes\n" ; \
+		false ; \
+	else \
+		printf "Binary size: $$SNAKE_EXEC_SIZE bytes\n" ; \
+	fi
+
+image:
+	@if [ -f $(OUTPUT_DIR)/$(OUTPUT) ] ; then \
+		SNAKE_EXEC_SIZE=$$(stat -L -c %s $(OUTPUT_DIR)/$(OUTPUT)) ; \
+		if [ $$SNAKE_EXEC_SIZE -gt 510 ] ; then \
+			printf "Binary is too large: $$SNAKE_EXEC_SIZE bytes\n" ; \
+			false ; \
+		else \
+			(rm -f $(OUTPUT_DIR)/$(OUTPUT_IMG) $(OUTPUT_DIR)/$(OUTPUT_BS) ) ; \
+			(cp $(OUTPUT_DIR)/$(OUTPUT) $(OUTPUT_DIR)/$(OUTPUT_BS) ) ; \
+			(sync $(OUTPUT_DIR)/$(OUTPUT_BS) ) ; \
+			(dd conv=notrunc if=/dev/zero of=$(OUTPUT_DIR)/$(OUTPUT_BS) bs=1 count=$$(( 510 - $$SNAKE_EXEC_SIZE )) seek=$$SNAKE_EXEC_SIZE > /dev/null 2>&1 & ) ; \
+			(sync $(OUTPUT_DIR)/$(OUTPUT_BS) ) ; \
+			(printf "\x55\xAA" >> $(OUTPUT_DIR)/$(OUTPUT_BS) ) ; \
+			(sync $(OUTPUT_DIR)/$(OUTPUT_BS) ) ; \
+			(cp $(OUTPUT_DIR)/$(OUTPUT_BS) $(OUTPUT_DIR)/$(OUTPUT_IMG) ) ; \
+			(sync $(OUTPUT_DIR)/$(OUTPUT_IMG) ) ; \
+			(dd conv=notrunc if=/dev/zero of=$(OUTPUT_DIR)/$(OUTPUT_IMG) bs=512 count=2879 seek=1 > /dev/null 2>&1 & ) ; \
+			(sync $(OUTPUT_DIR)/$(OUTPUT_IMG) ) ; \
+			(printf "Boot sector data created:              $(OUTPUT_DIR)/$(OUTPUT_BS)\n" ) ; \
+			(printf "Bootable 1.44MB floppy image created:  $(OUTPUT_DIR)/$(OUTPUT_IMG)\n" ) ; \
+		fi \
+	else \
+		printf "The binary file does not exist. Use 'make' to build it first!\n" ; \
+		false ; \
+	fi
+
+$(OBJECT):
+	@$(CXX) $(CXX_FLAGS) -o $(OBJECT_DIR)/$(OBJECT) $(SOURCE_DIR)/$(SOURCE)
+	@strip -S --strip-unneeded --remove-section=.note.gnu.gold-version --remove-section=.comment --remove-section=.note --remove-section=.note.gnu.build-id --remove-section=.note.ABI-tag $(OBJECT_DIR)/$(OBJECT)
+
+clean:
+	@rm -f $(OBJECT_DIR)/$(OBJECT) $(OUTPUT_DIR)/$(OUTPUT) $(OUTPUT_DIR)/$(OUTPUT_BS) $(OUTPUT_DIR)/$(OUTPUT_IMG)
---
+++ src/mbr_snake.cpp
@@ -0,0 +1,397 @@
+#include <cstdint>
+#include <iterator>
+#include <algorithm>
+#include <type_traits>
+
+// Don't use macros kids
+// Only doing this because I edit the source on Windows, and GCC-specific things break intellisense
+#if defined(__GNUG__) && !defined(__clang__)
+	#define _ALWAYS_INLINE_ __attribute__((always_inline))
+	#define _ASM_VOLATILE_(...) asm volatile(__VA_ARGS__)
+	#define _ASM_VOLATILE_GOTO_(...) asm volatile goto(__VA_ARGS__)
+#else
+	#define _ALWAYS_INLINE_
+	#define _ASM_VOLATILE_(...)
+	#define _ASM_VOLATILE_GOTO_(...)
+#endif
+
+template <typename T>
+struct vec2
+{
+	using value_type = T;
+	T x{};
+	T y{};
+	inline constexpr bool operator==(const vec2 &rhs) const
+	{
+		return (x == rhs.x) && (y == rhs.y);
+	}
+};
+
+using u16Vec2 = vec2<uint16_t>;
+
+class x86
+{
+	struct rgbColor
+	{
+		uint8_t r{};
+		uint8_t g{};
+		uint8_t b{};
+	};
+
+	template <uint8_t Red, uint8_t Green, uint8_t Blue>
+	static constexpr bool colorComp(const rgbColor &a, const rgbColor &b)
+	{
+		return (((a.r - Red) * (a.r - Red) + (a.g - Green) * (a.g - Green) + (a.b - Blue) * (a.b - Blue)) <
+				((b.r - Red) * (b.r - Red) + (b.g - Green) * (b.g - Green) + (b.b - Blue) * (b.b - Blue)));
+	}
+
+public:
+
+	// Only using the low word of the tick count for this project
+	// Using all 32 bits increases the binary size a lot
+	using ticks_t = uint16_t;
+
+	// Hard coded for 16-color video modes such as 0x0d
+	// Inspired by: https://www.youtube.com/watch?v=zBkNBP00wJE
+	template <uint8_t Red, uint8_t Green, uint8_t Blue>
+	static constexpr uint8_t rgb()
+	{
+		const rgbColor colorTable[] =
+		{
+			rgbColor{0, 0, 0},
+			rgbColor{0, 0, 85},
+			rgbColor{0, 85, 0},
+			rgbColor{0, 85, 85},
+			rgbColor{85, 0, 0},
+			rgbColor{85, 0, 85},
+			rgbColor{85, 85, 0},
+			rgbColor{170, 170, 170},
+			rgbColor{85, 85, 85},
+			rgbColor{0, 0, 170},
+			rgbColor{0, 170, 0},
+			rgbColor{0, 170, 170},
+			rgbColor{170, 0, 0},
+			rgbColor{170, 0, 170},
+			rgbColor{170, 170, 85},
+			rgbColor{255, 255, 255}
+		};
+		
+		return std::distance
+		(
+			std::begin(colorTable),
+			std::min_element
+			(
+				std::begin(colorTable),
+				std::end(colorTable),
+				colorComp<Red, Green, Blue>
+			)
+		);
+	}
+
+	// Not used in this project
+
+	// static void printChar(char c, uint8_t color = 7)
+	// {
+	// 	_ASM_VOLATILE_
+	// 	(
+	// 		"int $0x10"
+	// 		:
+	// 		: "ax"(0x0e00 | c), "bx"(color)
+	// 	);
+	// }
+
+	// static void printString(const char *c, uint8_t color = 7)
+	// {
+	// 	while (*c)
+	// 	{
+	// 		printChar(*c, color);
+	// 		std::advance(c, 1);
+	// 	}
+	// }
+
+	// static auto readKeyboardChar()
+	// {
+	// 	int16_t ax{};
+	// 	_ASM_VOLATILE_
+	// 	(
+	// 		"int $0x16"
+	// 		: "+ax"(ax)
+	// 	);
+	// 	return *reinterpret_cast<char*>(&ax); // Low byte
+	// }
+
+	static auto readKeyboardScanCode()
+	{
+		int16_t ax{};
+		_ASM_VOLATILE_
+		(
+			"int $0x16"
+			: "+ax"(ax)
+		);
+		return *(reinterpret_cast<int8_t*>(&ax) + 1); // High byte
+	}
+
+	[[nodiscard]] static auto isKeyAvailable()
+	{
+		_ASM_VOLATILE_GOTO_
+		(
+			"movb $0x01, %%ah\n\t"
+			"int $0x16\n\t"
+			"jz %l[ret_false]"
+			:
+			:
+			:
+			: ret_false
+		);
+		return true;
+		ret_false:
+		return false;
+	}
+
+	static void setPixel(uint8_t color, uint16_t x, uint16_t y)
+	{
+		_ASM_VOLATILE_
+		(
+			"xor %%bh, %%bh\n\t"
+			"int $0x10"
+			:
+			: "ax"(0x0c00 | color), "cx"(x), "dx"(y)
+		);
+	}
+
+	static void setVideoMode(uint16_t mode)
+	{
+		_ASM_VOLATILE_
+		(
+			"int $0x10"
+			:
+			: "ax"(mode)
+		);
+	}
+
+	[[nodiscard]] static auto getTicks()
+	{
+		uint16_t ticks[2];
+		_ASM_VOLATILE_
+		(
+			"int $0x1a"
+			: [high_word] "=cx"(ticks[1]), [low_word] "=dx"(ticks[0])
+			: "ax"(0x0)
+		);
+
+		// return ticks_t(ticks[0]) | (ticks_t(ticks[1]) << 16); // If we used all 32 bits
+		return ticks[0];
+	}
+
+	static void sleepUntil(ticks_t target)
+	{
+		while (getTicks() < target)
+		{
+		}
+	}
+
+	static void sleepFor(ticks_t duration)
+	{
+		sleepUntil(getTicks() + duration);
+	}
+};
+
+template <uint16_t Size>
+class game
+{
+	using dir_type = std::invoke_result_t<decltype(x86::readKeyboardScanCode)>;
+	using len_type = int8_t;
+
+	// Not using enum class on purpose
+	enum dirKeys : dir_type {
+		UP		= 0x48,
+		LEFT	= 0x4B,
+		RIGHT	= 0x4D,
+		DOWN	= 0x50
+	};
+
+	// segment[0] is always the head
+	// A raw ptr is a bit ugly, but a function taking an index and returning a reference adds to the binary size somehow
+	u16Vec2* const segment{ reinterpret_cast<decltype(segment)>(0x00007E00) };
+
+	dir_type	m_dir{};
+	len_type	m_len{}, m_nextLen{};
+	u16Vec2		m_foodPos{};
+	uint8_t		m_rndOffset{};
+
+	template <typename T>
+	[[nodiscard]] T randomData()
+	{
+		// lol
+		return *reinterpret_cast<T*>(0x7C00 + m_rndOffset++);
+	}
+
+	void spawnFood()
+	{
+		auto ticks	= x86::getTicks();
+		auto rnd	= randomData<x86::ticks_t>();
+		m_foodPos.x = (rnd ^ ticks) % Size;
+		m_foodPos.y = rnd % Size;
+	}
+
+	void reset()
+	{
+		m_len		= 1;
+		m_nextLen	= 1;
+		segment[0]	= { Size / 2u, Size / 2u };
+	}
+
+	void readInput()
+	{
+		if (x86::isKeyAvailable())
+		{
+			auto newInput = x86::readKeyboardScanCode();
+
+			auto isSameAxis = [](const auto& dir1, const auto& dir2)
+			{
+				// LSB is the same between arrow keys on the same axis (left/right and up/down)
+				return (dir1 & 0b1) == (dir2 & 0b1);
+			};
+
+			auto isValidInput = [&](const auto& input)
+			{
+				return (input >= dirKeys::UP && input <= dirKeys::DOWN);
+			};
+
+			// Validating input and preventing a 180° direction change if the length is over 1
+			if (isValidInput(newInput))
+			{
+				if (m_len == 1 || !isSameAxis(m_dir, newInput))
+				{
+					m_dir = newInput;
+				}
+			}
+		}
+	}
+
+	void updateHead()
+	{
+		// A switch makes for a larger binary here
+			 if (m_dir == dirKeys::UP)		segment[0].y--;
+		else if (m_dir == dirKeys::LEFT)	segment[0].x--;
+		else if (m_dir == dirKeys::RIGHT)	segment[0].x++;
+		else if (m_dir == dirKeys::DOWN)	segment[0].y++;
+	}
+
+public:
+
+	game() : m_foodPos{ Size / 5u, Size / 3u }, m_dir{ dirKeys::RIGHT }
+	{
+		x86::setVideoMode(0x0d);
+		reset();
+	}
+
+	auto scopedFrame(x86::ticks_t duration) const
+	{
+		class Frame
+		{
+			x86::ticks_t startTime{}, endTime{};
+		public:
+			Frame(x86::ticks_t time) : startTime{ x86::getTicks() }, endTime( endTime = startTime + time )
+			{
+			}
+			~Frame()
+			{
+				x86::sleepUntil(endTime);
+			}
+		};
+
+		return Frame(duration);
+	}
+
+	_ALWAYS_INLINE_ void update()
+	{
+		m_len = m_nextLen;
+
+		// Reading input if available
+		readInput();
+
+		// Moving the tail by setting each segment (except the head) to the position of the segment in front of it
+		for (len_type i = m_len - 1; i > 0; i--)
+		{
+			segment[i] = segment[i - 1];
+		}
+
+		// Moving the head to its new position
+		updateHead();
+
+		auto isCoordInBounds	= []	(const auto& val) { return val <= (Size - 1u);	};
+		auto isHeadAtPosition	= [this](const auto& pos) { return pos == segment[0];	};
+
+		// Resetting on edge collision
+		if (!isCoordInBounds(segment[0].x) ||
+			!isCoordInBounds(segment[0].y))
+		{
+			reset();
+			return;
+		}
+
+		// Resetting on self-collision
+		for (len_type i{ 1 }; i < m_len; i++)
+		{
+			if (isHeadAtPosition(segment[i]))
+			{
+				reset();
+				return;
+			}
+		}
+
+		// Checking if the food has been reached
+		if (isHeadAtPosition(m_foodPos))
+		{
+			// Only effective on the next frame so that we don't render an uninitialized segment on the current frame
+			m_nextLen++;
+
+			spawnFood();
+		}
+	}
+
+	void draw() const
+	{
+		// Play area
+		for (uint16_t i{}; i < Size; i++)
+		{
+			for (uint16_t j{}; j < Size; j++)
+			{
+				x86::setPixel(x86::rgb<64, 64, 64>(), i, j);
+			}
+		}
+
+		// Snake
+		for (len_type i{}; i < m_len; i++)
+		{
+			x86::setPixel(x86::rgb<0, 255, 0>(), segment[i].x, segment[i].y);
+		}
+
+		// Food
+		x86::setPixel(x86::rgb<255, 0, 0>(), m_foodPos.x, m_foodPos.y);
+	}
+
+	_ALWAYS_INLINE_ void mainLoop()
+	{
+		update();
+		draw();
+	}
+};
+
+int main()
+{
+	game<16> snake;
+
+	// 2 ticks per frame is about 9.1 FPS
+	// If the game is too fast, set it to 3 which is about 6.1 FPS (shouldn't affect the binary size)
+	constexpr auto frameTime = x86::ticks_t(2);
+
+	while (true)
+	{
+		auto frame = snake.scopedFrame(frameTime);
+		snake.mainLoop();
+	}
+
+	return 0;
+}
